723c4b4ff11c23e3379e8b4c39f3518e6ae7e80a
Gopi-Yadav <gopiyadav2918@gmail.com>
2023-07-02 15:05:02 +0530

    first commit

--- src/app/home/home.page.ts
+++ src/app/home/home.page.ts
@@ -6,7 +6,36 @@ import { Component } from '@angular/core';
   styleUrls: ['home.page.scss'],
 })
 export class HomePage {
+  location: string | undefined;
+  checkInDate: string | undefined;
+  checkOutDate: string | undefined;
+  guests: number | undefined;
+  hotels: any[] = [];
 
-  constructor() {}
-
+  searchHotels() {
+    
+    this.hotels = [
+      {
+        name: 'Novotel',
+        location: 'Hyderabad',
+        image: 'assets/nova.jpg',
+        price: 10000,
+        rating: 4.5,
+      },
+      {
+        name: 'Hotel Grand mercure',
+        location: 'Bangalore',
+        image: 'assets/mercure.jpg',
+        price: 5000,
+        rating: 4.0,
+      },
+      {
+        name: 'ibis Chennai OMR',
+        location: 'Chennai',
+        image: 'assets/ibis.jpg',
+        price: 3000,
+        rating: 4.2,
+      },
+    ];
+  }
 }
